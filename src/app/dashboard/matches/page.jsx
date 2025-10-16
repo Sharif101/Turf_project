3ad1e712ca41879453366e8134796d48ae889fe8
Sharif101 <sharif009009@gmail.com>
2025-10-16 15:57:41 +0600

    create match and dashboard page

---
+++ src/app/dashboard/matches/page.jsx
@@ -0,0 +1,10 @@
+import DashboardLayout from "@/components/DashboardLayout/DashboardLayout";
+import Matches from "@/components/Matches/Matches";
+
+export default function MatchesPage() {
+  return (
+    <DashboardLayout>
+      <Matches />
+    </DashboardLayout>
+  );
+}
---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,10 @@
+import Dashboard from "@/components/Dashboard/Dashboard";
+import DashboardLayout from "@/components/DashboardLayout/DashboardLayout";
+
+export default function DashboardPage() {
+  return (
+    <DashboardLayout>
+      <Dashboard />
+    </DashboardLayout>
+  );
+}
